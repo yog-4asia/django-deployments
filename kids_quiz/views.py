2b78814104bf537bfd4700b7ac312256f27880d1
yog-4asia <yogesh.chaurasia@ymail.com>
2020-05-10 00:03:39 +0530

    Fresh Start

--- kids_quiz/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def quiz_home(request):
-    return render (request,"quiz_home.html")
--- kids_tutorial/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KidsTutorialConfig(AppConfig):
-    name = 'kids_tutorial'
